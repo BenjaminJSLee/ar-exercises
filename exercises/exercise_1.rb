692973fa799af2a76483393ffe551313e83e0eee
Benjamin Lee <benjaminjslee@gmail.com>
2021-01-04 22:41:51 +0000

    Finish exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,24 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.new
+burnaby.annual_revenue = 300000
+burnaby.name = "Burnaby"
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
+
+richmond = Store.new
+richmond.annual_revenue = 1260000
+richmond.name = "Richmond"
+richmond.mens_apparel = false
+richmond.womens_apparel = true
+richmond.save
+
+gastown = Store.new
+gastown.annual_revenue = 190000
+gastown.name = "Gastown"
+gastown.mens_apparel = true
+gastown.womens_apparel = false
+gastown.save
