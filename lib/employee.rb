663e4af4dca0a25ccc71c5d56da1c23e2b54d483
Benjamin Lee <benjaminjslee@gmail.com>
2021-01-05 01:17:03 +0000

    Finish exercises 8 & 9

--- lib/employee.rb
+++ lib/employee.rb
@@ -8,4 +8,17 @@ class Employee < ActiveRecord::Base
     less_than: 200
   }
   validates_associated :store
+  before_create :create_password
+
+  private
+
+  def create_password
+    password = ""
+    chars = [('a'..'z'), ('A'..'Z'), ('0'..'9')].map(&:to_a).flatten
+    (0...8).map {
+      password += chars[rand(0...chars.length)]
+    }
+    self.password = password
+  end
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -5,10 +5,23 @@ class Store < ActiveRecord::Base
     only_integer: true, 
     greater_than_or_equal_to: 0
   }
-  validate :must_have_woman_or_man_apparel
-  def must_have_woman_or_man_apparel
+  validate :has_womens_or_mens_apparel
+
+  before_destroy :has_employees
+
+  private
+
+  def has_womens_or_mens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:store, "must have at least one type of apparel")
     end
   end
+
+  def has_employees
+    if employees.where(store_id: self.id).count > 0
+      return false
+    end
+    return true
+  end
+
 end
